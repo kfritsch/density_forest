5c6287381cdb32dafb205c39fb4eb885bd1a5992
kfritsch <kai.m.fritsch@googlemail.com>
2017-07-02 12:16:32 +0200

    added densityforest.py

--- DensityTree.py
+++ DensityTree.py
@@ -3,7 +3,7 @@
 
 # In[83]:
 
-print(1)
+# print(1)
 
 
 # In[20]:
@@ -14,38 +14,46 @@ import threading as trd
 import math
 import random as rnd
 import copy
-    
+
 
 #def estimate_gaussian(data):
  #   return np.mean(data),np.cov(data)
-    
-    
+
+
 def info_gain(data,cov,data_l,cov_l,data_r,cov_r):
-    
+
     a=np.linalg.det(cov)
     if(isnan(a)==True):
         a=0.000000000000000000001
     a=np.log(abs(a))
-    
-    
+
+
     b=np.linalg.det(cov_l)
+    if(b==0):
+        print(data_l)
+        print(len(data_l))
+        print(cov_l)
     if np.isnan(b)==True:
         b=0.000000000000000000001
     #print(data_l.shape[1])
     b=(data_l.shape[0]/data.shape[0]) * np.log(abs(b))
-    
-    
+
+
     c=np.linalg.det(cov_r)
+    if(c==0):
+        print(data_r)
+        print(len(data_r))
+        print(cov_r)
     if np.isnan(c)==True:
         c=0.000000000000000000001
     c=(data_r.shape[0]/data.shape[0]) * np.log(abs(c))
-    
-    
-    print('b: '+str(b) +' shape: '+str(data_l.shape))
-    print('c: '+str(c) +' shape: '+str(data_r.shape))
-    print('infogain: '+str(a-b-c))
+
+
+    # print('b: '+str(b) +' shape: '+str(data_l.shape))
+    # print('c: '+str(c) +' shape: '+str(data_r.shape))
+    # print('infogain: '+str(a-b-c))
     return a-b-c
-    
+
 def split_data(data,split,direction):
 #    print('hi')
     left_data=np.array([[0,0]])
@@ -56,12 +64,12 @@ def split_data(data,split,direction):
         else:
             #print(1,left_data[d].shape[0])
             right_data=np.append(right_data,np.reshape(data[d],(1,data[d].shape[0])),axis=0)
-            
+
    # print(left_data)
     left_data=np.delete(left_data,0,axis=0)
     right_data=np.delete(right_data,0,axis=0)
     return(left_data,right_data)
-            
+
 
 class RandomDensityTree:
 
@@ -74,9 +82,9 @@ class RandomDensityTree:
         self.tree=tree
         self.min_infogain=min_infogain
         self.num_splits=num_splits
-        
+
     def fit(self,data,axis=0):
-        if(axis=1):
+        if(axis==1):
             data=np.transpose(data)
         self.root=data
         self.mean = np.mean(data,axis=0)
@@ -90,7 +98,7 @@ class RandomDensityTree:
             new.append(self.rootnode.predict(p))
         new=np.array(new)
         return new
-    
+
     def max_prob():
         leafs=self.leaf_nodes()
         probs=[]
@@ -102,7 +110,7 @@ class RandomDensityTree:
         means=[]
         self.rootnode.get_means(means)
         return means
-    
+
     def get_split_info(self):
         histories=[]
         self.rootnode.get_split_info(means)
@@ -113,23 +121,23 @@ class RandomDensityTree:
         leafs=[]
         self.rootnode.leaf_nodes(leafs)
         return np.array(leafs)
-    
+
 class Node:
-    
+
     def __init__(self,data,cov,history,tree,num_splits,min_infogain,max_depth,pointer):
 
         #self.min_infgain=min_infgain
         self.maxdepth=max_depth
         self.min_infogain=min_infogain
         self.pointer=pointer
-        
+
         self.tree=tree
         self.num_splits=num_splits
     #    tree[pointer]={''}
         self.split=float('nan')
         self.split_dim=float('nan')
         self.history=copy.deepcopy(history)
-        
+
         self.root=data
         self.mean=np.mean(data,axis=0)
         self.cov=cov
@@ -138,9 +146,9 @@ class Node:
         self.right_child=float('nan')
         if(max_depth==0 or data.shape[1]==1):
             self.isLeaf=True
-           
+
         else:
-            
+
             rnd_splits=[]
             for dim in range(int(num_splits)):
                 direction=rnd.choice([0,1])
@@ -155,10 +163,10 @@ class Node:
             covs_right=[]
 
             for s in range(num_splits):#or the number of random splits
-                
+
                 left_data,right_data=split_data(data,rnd_splits[s]['split'],rnd_splits[s]['direction'])
                 if(left_data.shape[0]>2 and right_data.shape[0]>2):
-                    print('split ' +str(rnd_splits[s]['split'])+'  direction: '+str(rnd_splits[s]['direction']))
+                    # print('split ' +str(rnd_splits[s]['split'])+'  direction: '+str(rnd_splits[s]['direction']))
                    # left_data=[] #data left of the split
                    # right_data=[] #data right of the split
                     #for a in data:
@@ -171,6 +179,8 @@ class Node:
                     right_datas.append(right_data)
                     left_datas.append(left_data)
 
+                    if(len(left_data)==1 or len(right_data)==1):
+                        input()
                     cov_l=np.cov(np.transpose(left_data))
                     cov_r=np.cov(np.transpose(right_data))
 
@@ -186,17 +196,17 @@ class Node:
                     #information gain if this split is used
             if len(info_gains)==0:
                 self.isLeaf=True
-                   
+
             else:
-         
+
                 best=np.argmax(info_gains)
-                print('best infogain: '+str(info_gains[best]))
-                print(min_infogain)
+                # print('best infogain: '+str(info_gains[best]))
+                # print(min_infogain)
                 if info_gains[best] >= min_infogain:
-             
+
                     self.split=rnd_splits[best]['split']   #best split
-                    self.split_dim=rnd_splits[best]['direction'] 
-                    print('history:'+str(history))
+                    self.split_dim=rnd_splits[best]['direction']
+                    # print('history:'+str(history))
                     self.history.append(rnd_splits[best])
                     if(2*pointer+2>=len(tree)):
                         for i in range((2*pointer+2)-len(tree)+1):
@@ -208,18 +218,18 @@ class Node:
                     leftnode=Node(left_datas[best],covs_left[best],self.history,tree,self.num_splits,self.min_infogain,self.maxdepth-1,2*pointer+1)
                     tree[2*pointer+1]=leftnode
                     self.left_child=leftnode
-            
+
                    # print(len(right_datas[best],covs_right[best],history,self.num_splits,self.min_infogain,s2))
-                    
+
                     self.history[len(self.history)-1]['child']='right'
                     rightnode=Node(right_datas[best],covs_right[best],self.history,tree,self.num_splits,self.min_infogain,self.maxdepth-1,2*pointer+2)
                     tree[2*pointer+2]=rightnode
                     self.right_child=rightnode
-                    
+
                 else:
-         
+
                     self.isLeaf=True
-        print((self.isLeaf,self.left_child))
+        # print((self.isLeaf,self.left_child))
     def predict(self,point):
         if self.isLeaf==True:
             return self.pointer
@@ -228,7 +238,7 @@ class Node:
                 return self.left_child.predict(point)
             else:
                 return self.right_child.predict(point)
-    '''         
+    '''
      def get_split_info(self,histories):
         if(self.isLeaf==True):
             return self.history
@@ -249,10 +259,10 @@ class Node:
         else:
             self.left_child.get_means(means)
             self.right_child.get_means(means)
-            
+
     def maxprob():
         return 1
-    
+
     def leaf_nodes(self,leafs):
       #  if self.left_child.isLeaf == True:
        #     leafs.append(self.left_child.pointer)
@@ -264,10 +274,10 @@ class Node:
        #     leafs.append(self.right_child)
         #else:
             self.right_child.leaf_nodes(leafs)
-        
+
     def isnan(self):
         return False
-    
+
 def partition_function(tree, x):
     # generate a lot of samples in the bounds of the data and the size of the bounded shape
     samples, b_size = generate_monte_carlo_sample(x)
@@ -287,7 +297,7 @@ def partition_function(tree, x):
         g_probs = mnd(samples[sample_id_mask])
         g_cnt = np.sum(g_probs_samples<=g_probs)
         g_ints[ln_id] = g_cnt/len(samples)*b_size
-    
+
 
 
 def generate_monte_carlo_sample(X, num_samples=1000000):
@@ -299,151 +309,148 @@ def generate_monte_carlo_sample(X, num_samples=1000000):
     d_maxs = np.max(X,axis=0)
     samples = np.add(np.multiply(samples,d_maxs-d_mins),d_mins)
     b_size = np.prod(d_maxs-d_mins)
-    return samples, b_size    
+    return samples, b_size
 '''
 class RandomDensityForest:
     def _init(self,max_length=10,num_trees=10):
         self.max_length=max_length
         self.num_trees=num_trees
-        
+
     def train_and_stuff(data):
         #lauch DensityTrees
         #train on data
         #return results
         #use partition function
         #combine them into final result
-    
+
     I= entropy - sum
-theta= max(I)    
+theta= max(I)
 '''
 
-
-# In[23]:
-
-import numpy as np
-print(DensityTree.get_means())
-
-
-# In[26]:
-
-print(data.shape)
-print(np.transpose(data))
-
-
-# In[22]:
-
-get_ipython().magic('matplotlib inline')
-import matplotlib.pyplot as plt
 #
-
-data = np.zeros([100,2])
-for i in range(30):
-    data[i]=np.random.normal([2,2],0.5)
-   # data[i][0]=np.random.normal(2,0.5)[0]
-    #data[i][1]=np.random.normal(2)[1]
-for i in range(30):
-    data[30+i]=np.random.normal([5,5],0.5)
-    #data[30+i][1]=np.random.normal(5,0.5) 
-for i in range(30):
-    data[60+i]=np.random.normal([5,1],0.5)
-   
-for i in range(10):
-    data[90+i]=np.random.normal([1,5],0.5)
-
-
-DensityTree=RandomDensityTree()
-DensityTree.fit(data)
-
-print(DensityTree.leaf
-# In[15]:
-
-
-DensityTree2=RandomDensityTree()
-DensityTree2.fit(data)
-DensityTree2.predict([[2,3],[4,5]])
-
-
-# In[16]:
-
-print(partition_function(tree,100))
-
-
-# In[10]:
-
-nodes=DensityTree.leaf_nodes()
-tree=DensityTree.tree
-plt.plot(data[:,0],data[:,1], "o")
-for d in nodes:
-    print(tree[d].history)
-    print(tree[d].root.shape)
-    plt.plot(tree[d].mean[0],tree[d].mean[1],'o',color='r')
- #   lines=d.history
-  #  if lines['direction']==0:
-   #     plot
-   # plt.savefig('results')
-plt.plot([3.4611913424559244,3.4611913424559244],[0,6])
-#plt.plot([0,6],[ 4.0724581571601854, 4.0724581571601854])
-plt.plot([0,6],[ 2.2392637110032361, 2.2392637110032361])
-plt.plot([0,6],[ 2.6017827096744832,  2.6017827096744832])
-
-plt.plot([0,6],[4.3153900996135484,  4.3153900996135484])
-
-plt.plot([0,6],[ 3.3000801823937387, 3.3000801823937387])
-
-#plt.plot( 0.86719180226239123, 'direction': 0)
-#plt.plot([2.0024517583718917,2.0024517583718917],[-1,6])
-#plt.plot([0.9883004157571309,0.9883004157571309],[-1,6])
-
-
-#plt.plot( 4.64157772,  0.63988413,"o")
-#plt.plot( 3.03334449,  2.46542821,"o")
-#plt.plot( 4.24936537,  5.14213751,"o")
-#plt.plot( 3.23164995,  5.9834305 ,"o")
-
-
-# In[194]:
-
-nodes=DensityTree.leaf_nodes()
-plt.plot(data[:,0],data[:,1], "o")
-for d in nodes:
-    print(d.history)
-    print(d.root.shape)
-    plt.plot(d.mean[0],d.mean[1],'o',color='r')
-    lines=d.history
-    if lines['direction']==0:
-        plot
-    plt.savefig('results')
-plt.plot([0,6],[5.358583094356550,5.358583094356550])
-#plt.plot([0,6],[ 4.0724581571601854, 4.0724581571601854])
-plt.plot([0,6],[  3.245427000841719,  3.245427000841719])
-plt.plot([  0.86719180226239123,  0.86719180226239123],[0,6])
-plt.plot([0,6],[ 5.7306771060727044,  5.7306771060727044])
-#plt.plot( 0.86719180226239123, 'direction': 0)
-#plt.plot([2.0024517583718917,2.0024517583718917],[-1,6])
-#plt.plot([0.9883004157571309,0.9883004157571309],[-1,6])
-
-
-#plt.plot( 4.64157772,  0.63988413,"o")
-#plt.plot( 3.03334449,  2.46542821,"o")
-#plt.plot( 4.24936537,  5.14213751,"o")
-#plt.plot( 3.23164995,  5.9834305 ,"o")
-
-
-# In[181]:
-
-data2 = np.zeros([100,2])
-for i in range(100):
-    data2[i]=np.random.normal([2,2],1)
-    
-print(np.linalg.det(np.cov(np.transpose(data2))))
-
-
-# In[19]:
-
-print(np.log(-1))
-
-
-# In[ ]:
-
-
-
+# # In[23]:
+#
+# import numpy as np
+# print(DensityTree.get_means())
+#
+#
+# # In[26]:b
+#
+# print(data.shape)
+# print(np.transpose(data))
+#
+#
+# # In[22]:
+#
+# get_ipython().magic('matplotlib inline')
+# import matplotlib.pyplot as plt
+# #
+#
+# data = np.zeros([100,2])
+# for i in range(30):
+#     data[i]=np.random.normal([2,2],0.5)
+#    # data[i][0]=np.random.normal(2,0.5)[0]
+#     #data[i][1]=np.random.normal(2)[1]
+# for i in range(30):
+#     data[30+i]=np.random.normal([5,5],0.5)
+#     #data[30+i][1]=np.random.normal(5,0.5)
+# for i in range(30):
+#     data[60+i]=np.random.normal([5,1],0.5)
+#
+# for i in range(10):
+#     data[90+i]=np.random.normal([1,5],0.5)
+#
+#
+# DensityTree=RandomDensityTree()
+# DensityTree.fit(data)
+#
+# print(DensityTree.leaf
+# # In[15]:
+#
+#
+# DensityTree2=RandomDensityTree()
+# DensityTree2.fit(data)
+# DensityTree2.predict([[2,3],[4,5]])
+#
+#
+# # In[16]:
+#
+# print(partition_function(tree,100))
+#
+#
+# # In[10]:
+#
+# nodes=DensityTree.leaf_nodes()
+# tree=DensityTree.tree
+# plt.plot(data[:,0],data[:,1], "o")
+# for d in nodes:
+#     print(tree[d].history)
+#     print(tree[d].root.shape)
+#     plt.plot(tree[d].mean[0],tree[d].mean[1],'o',color='r')
+#  #   lines=d.history
+#   #  if lines['direction']==0:
+#    #     plot
+#    # plt.savefig('results')
+# plt.plot([3.4611913424559244,3.4611913424559244],[0,6])
+# #plt.plot([0,6],[ 4.0724581571601854, 4.0724581571601854])
+# plt.plot([0,6],[ 2.2392637110032361, 2.2392637110032361])
+# plt.plot([0,6],[ 2.6017827096744832,  2.6017827096744832])
+#
+# plt.plot([0,6],[4.3153900996135484,  4.3153900996135484])
+#
+# plt.plot([0,6],[ 3.3000801823937387, 3.3000801823937387])
+#
+# #plt.plot( 0.86719180226239123, 'direction': 0)
+# #plt.plot([2.0024517583718917,2.0024517583718917],[-1,6])
+# #plt.plot([0.9883004157571309,0.9883004157571309],[-1,6])
+#
+#
+# #plt.plot( 4.64157772,  0.63988413,"o")
+# #plt.plot( 3.03334449,  2.46542821,"o")
+# #plt.plot( 4.24936537,  5.14213751,"o")
+# #plt.plot( 3.23164995,  5.9834305 ,"o")
+#
+#
+# # In[194]:
+#
+# nodes=DensityTree.leaf_nodes()
+# plt.plot(data[:,0],data[:,1], "o")
+# for d in nodes:
+#     print(d.history)
+#     print(d.root.shape)
+#     plt.plot(d.mean[0],d.mean[1],'o',color='r')
+#     lines=d.history
+#     if lines['direction']==0:
+#         plot
+#     plt.savefig('results')
+# plt.plot([0,6],[5.358583094356550,5.358583094356550])
+# #plt.plot([0,6],[ 4.0724581571601854, 4.0724581571601854])
+# plt.plot([0,6],[  3.245427000841719,  3.245427000841719])
+# plt.plot([  0.86719180226239123,  0.86719180226239123],[0,6])
+# plt.plot([0,6],[ 5.7306771060727044,  5.7306771060727044])
+# #plt.plot( 0.86719180226239123, 'direction': 0)
+# #plt.plot([2.0024517583718917,2.0024517583718917],[-1,6])
+# #plt.plot([0.9883004157571309,0.9883004157571309],[-1,6])
+#
+#
+# #plt.plot( 4.64157772,  0.63988413,"o")
+# #plt.plot( 3.03334449,  2.46542821,"o")
+# #plt.plot( 4.24936537,  5.14213751,"o")
+# #plt.plot( 3.23164995,  5.9834305 ,"o")
+#
+#
+# # In[181]:
+#
+# data2 = np.zeros([100,2])
+# for i in range(100):
+#     data2[i]=np.random.normal([2,2],1)
+#
+# print(np.linalg.det(np.cov(np.transpose(data2))))
+#
+#
+# # In[19]:
+#
+# print(np.log(-1))
+#
+#
+# # In[ ]:
